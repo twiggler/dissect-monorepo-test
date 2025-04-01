4353e28a64edce38a87fc13e56a5bee10bd1b117
Stefan de Reuver <9864602+Horofic@users.noreply.github.com>
2025-04-01 17:45:50 +0200

    Make chunk size configurable (fox-it/dissect.archive#13)

--- projects/dissect.archive/tests/conftest.py
+++ projects/dissect.archive/tests/conftest.py
@@ -25,20 +25,35 @@ def open_file_gz(name: str, mode: str = "rb") -> Iterator[BinaryIO]:
 
 
 @pytest.fixture
-def basic_wim() -> Iterator[BinaryIO]:
-    yield from open_file_gz("data/basic.wim.gz")
+def basic_wim_4k() -> Iterator[BinaryIO]:
+    yield from open_file_gz("_data/basic4k.wim.gz")
+
+
+@pytest.fixture
+def basic_wim_8k() -> Iterator[BinaryIO]:
+    yield from open_file_gz("_data/basic8k.wim.gz")
+
+
+@pytest.fixture
+def basic_wim_16k() -> Iterator[BinaryIO]:
+    yield from open_file_gz("_data/basic16k.wim.gz")
+
+
+@pytest.fixture
+def basic_wim_32k() -> Iterator[BinaryIO]:
+    yield from open_file_gz("_data/basic32k.wim.gz")
 
 
 @pytest.fixture
 def basic_vma() -> Iterator[BinaryIO]:
-    yield from open_file_gz("data/test.vma.gz")
+    yield from open_file_gz("_data/test.vma.gz")
 
 
 @pytest.fixture
 def vbk9() -> Iterator[BinaryIO]:
-    yield from open_file_gz("data/test9.vbk.gz")
+    yield from open_file_gz("_data/test9.vbk.gz")
 
 
 @pytest.fixture
 def vbk13() -> Iterator[BinaryIO]:
-    yield from open_file_gz("data/test13.vbk.gz")
+    yield from open_file_gz("_data/test13.vbk.gz")
--- projects/dissect.archive/tests/test_wim.py
+++ projects/dissect.archive/tests/test_wim.py
@@ -3,11 +3,32 @@ from __future__ import annotations
 import hashlib
 from typing import BinaryIO
 
-from dissect.archive.wim import WIM
+import pytest
+from dissect.util.compression.lzxpress_huffman import decompress
+
+from dissect.archive.wim import WIM, CompressedStream
 
 
-def test_wim(basic_wim: BinaryIO) -> None:
-    wim = WIM(basic_wim)
+@pytest.mark.parametrize(
+    ("fixture", "chunk_size"),
+    [
+        ("basic_wim_4k", 0x1000),
+        ("basic_wim_8k", 0x2000),
+        ("basic_wim_16k", 0x4000),
+        ("basic_wim_32k", 0x8000),
+    ],
+)
+def test_wim(fixture: BinaryIO, chunk_size: int, request: pytest.FixtureRequest) -> None:
+    value = request.getfixturevalue(fixture)
+    wim = WIM(value)
+    assert wim.header.CompressionSize == chunk_size
+
+    resource = next(iter(wim.resources.values()))
+    assert resource.open().chunk_size == chunk_size
+
+    stream = CompressedStream(wim.fh, resource.offset, resource.size, resource.original_size, decompress, chunk_size)
+    assert resource.wim.header.CompressionSize == stream.chunk_size
+    assert resource.open().read() == stream.read()
 
     images = list(wim.images())
     assert len(images) == 1
