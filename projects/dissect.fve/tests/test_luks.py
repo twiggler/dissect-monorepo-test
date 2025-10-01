4e6c27a03ec67101eb92e0d13c443f4647ec7ab6
Erik Schamper <1254028+Schamper@users.noreply.github.com>
2025-10-01 15:46:28 +0200

    Replace argon2-cffi with Rust (fox-it/dissect.fve#10)

--- projects/dissect.fve/tests/test_luks.py
+++ projects/dissect.fve/tests/test_luks.py
@@ -5,6 +5,7 @@ from typing import BinaryIO
 
 import pytest
 
+from dissect.fve.crypto import argon2
 from dissect.fve.luks.luks import LUKS
 from tests._utils import open_file_gz
 
@@ -29,16 +30,29 @@ def _verify_passphrase_crypto(test_file: BinaryIO, passphrase: str, cipher_type:
 @pytest.mark.parametrize(
     ("test_file", "password", "cipher"),
     [
-        ("_data/luks1/aes-ecb.bin.gz", "password", "aes-ecb"),
-        ("_data/luks1/sha1.bin.gz", "password", "aes-ecb"),
-        ("_data/luks2/aes-cbc-plain.bin.gz", "password", "aes-cbc-plain"),
-        ("_data/luks2/aes-cbc-essiv.bin.gz", "password", "aes-cbc-essiv:sha256"),
-        ("_data/luks2/aes-ecb-pbkdf2.bin.gz", "password", "aes-ecb"),
-        ("_data/luks2/aes-xts-plain64.bin.gz", "password", "aes-xts-plain64"),
-        ("_data/luks2/multiple-slots.bin.gz", "password", "aes-cbc-plain"),
-        ("_data/luks2/multiple-slots.bin.gz", "another", "aes-cbc-plain"),
+        pytest.param("_data/luks1/aes-ecb.bin.gz", "password", "aes-ecb", id="luks1-aes-ecb"),
+        pytest.param("_data/luks1/sha1.bin.gz", "password", "aes-ecb", id="luks1-aes-ecb-sha1"),
+        pytest.param("_data/luks2/aes-cbc-plain.bin.gz", "password", "aes-cbc-plain", id="luks2-aes-cbc-plain"),
+        pytest.param("_data/luks2/aes-cbc-essiv.bin.gz", "password", "aes-cbc-essiv:sha256", id="luks2-aes-cbc-essiv"),
+        pytest.param("_data/luks2/aes-ecb-pbkdf2.bin.gz", "password", "aes-ecb", id="luks2-aes-ecb-pbkdf2"),
+        pytest.param("_data/luks2/aes-xts-plain64.bin.gz", "password", "aes-xts-plain64", id="luks2-aes-xts-plain64"),
+        pytest.param("_data/luks2/multiple-slots.bin.gz", "password", "aes-cbc-plain", id="luks2-multiple-slots-1"),
+        pytest.param("_data/luks2/multiple-slots.bin.gz", "another", "aes-cbc-plain", id="luks2-multiple-slots-2"),
     ],
 )
-def test_luks(test_file: str, password: str, cipher: str) -> None:
+def test_luks(test_file: str, password: str, cipher: str, request: pytest.FixtureRequest) -> None:
+    if (
+        request.node.callspec.id
+        in (
+            "luks2-aes-cbc-plain",
+            "luks2-aes-cbc-essiv",
+            "luks2-aes-xts-plain64",
+            "luks2-multiple-slots-1",
+            "luks2-multiple-slots-2",
+        )
+        and not argon2.HAS_ARGON2
+    ):
+        pytest.skip("Argon2 is not available, skipping")
+
     with contextlib.contextmanager(open_file_gz)(test_file) as fh:
         _verify_passphrase_crypto(fh, password, cipher)
